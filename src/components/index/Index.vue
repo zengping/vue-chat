255d6b6f5ee0a6dc59f349fbe716c6cdcb7e950d
zengping <331962716@qq.com>
2016-10-24 23:06:14 +0800

    create new report

---
+++ src/components/index/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="index">
+    <heade></heade>
+    <list></list>
+  </div>
+</template>
+
+<script>
+import Heade from '../public/Heade'
+import List from './List'
+
+export default {
+  components: {
+    Heade,
+    List
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import { router } from './router-config'
+
+/* eslint-disable no-new */
+new Vue({
+  router
+}).$mount('#app')
