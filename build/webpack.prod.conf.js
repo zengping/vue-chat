1b14da6ac5adf86a59e8eb1236e47da9fa1bbf98
JOHN ZEN <JOHN ZEN>
2017-04-18 10:13:37 +0800

    modify

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -50,7 +50,23 @@ var webpackConfig = merge(baseWebpackConfig, {
         // https://github.com/kangax/html-minifier#options-quick-reference
       },
       // necessary to consistently work with multiple chunks via CommonsChunkPlugin
-      chunksSortMode: 'dependency'
+      chunksSortMode: 'dependency',
+      chunks: ['manifest', 'vendor', 'app']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'login.html',
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+        // more options:
+        // https://github.com/kangax/html-minifier#options-quick-reference
+      },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      chunksSortMode: 'dependency',
+      chunks: ['manifest', 'vendor', 'login']
     }),
     // split vendor js into its own file
     new webpack.optimize.CommonsChunkPlugin({
